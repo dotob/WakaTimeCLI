c04b43c0572b55fa4b48b353f379e5d76b67ba54
basti <sk@dotob.de>
2016-06-28 22:37:40 +0200

    no more typings in git

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/commander/index.d.ts" />
-/// <reference path="globals/lodash/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/require/index.d.ts" />
-/// <reference path="modules/chalk/index.d.ts" />
-/// <reference path="modules/request/index.d.ts" />
